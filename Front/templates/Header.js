4c610c34dfc2fb272d44b9bb2bb837c047204c69
cristian navarro leon <cristiannl.0117@gmail.com>
2021-10-11 19:12:33 -0500

    add header styles

--- Front/templates/Header.js
+++ Front/templates/Header.js
@@ -1,28 +1,28 @@
 const Header = () => {
     
     let nav = document.createElement('nav');
-    nav.classList.add("navbar" , "navbar-dark" , "bg-primary" ,"justify-content-between" , "px-5");
+    nav.classList.add("justify-content-between" , "px-5");
 
     const logo = `
     <span class="navbar-brand">LOGO</span>  
     `;
     
     const menu = `
-        <ul class="navbar-nav d-flex flex-row">
-            <li class="nav-item mx-4">
-                <a class="nav-link" href="#contacts">Contactos</a>
+        <ul class="navbar-nav d-flex flex-row g-0">
+            <li class="nav-item">
+                <a class="" href="#contacts">Contactos</a>
             </li>
-            <li class="nav-item mx-4">
-                <a class="nav-link" href="#companies">Compañias</a>
+            <li class="nav-item">
+                <a class="" href="#companies">Compañias</a>
             </li>
-            <li class="nav-item mx-4">
-                <a class="nav-link" href="#users">Usuarios</a>
+            <li class="nav-item li-users">
+                <a class="" href="#users">Usuarios</a>
             </li>
-            <li class="nav-item mx-4">
-                <a class="nav-link" href="#regions">Region / Ciudad</a>
+            <li class="nav-item li-regions">
+                <a class="" href="#regions">Region / Ciudad</a>
             </li>
-            <li class="nav-item mx-4">
-                <a class="nav-link" href="#">Salir</a>
+            <li class="nav-item li-logout">
+                <a class="" href="#">Salir</a>
             </li>
         </ul>
     `;
