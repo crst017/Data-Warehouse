5fbb4914965a200f53c34526dd3afb0154034e9c
cristian navarro leon <cristiannl.0117@gmail.com>
2021-10-16 19:37:59 -0500

    mod getToken return, now returns auth json

--- Front/utils/getToken.js
+++ Front/utils/getToken.js
@@ -1,3 +1,13 @@
-const getToken = () => sessionStorage.getItem('token') || null;    
+const getToken = () => {
+    
+    const token = sessionStorage.getItem('token') || null;
+
+    const header = {
+        'Authorization' : `Bearer ${token}`
+    }
+    
+    return header
+}
+
 
 export default getToken;
