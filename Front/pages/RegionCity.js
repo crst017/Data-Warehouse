e07af48a75beae0b589cd4e368f495d7981977d3
cristian navarro leon <cristiannl.0117@gmail.com>
2021-11-09 00:52:48 -0500

    add entire region, country, city tree

--- Front/pages/RegionCity.js
+++ Front/pages/RegionCity.js
@@ -1,5 +1,8 @@
 import AccordionItem from "../templates/AccordionItem.js";
 import region from "../utils/handleRegions.js";
+import country from "../utils/handleCountries.js";
+import city from "../utils/handleCities.js";
+
 import CreateRegionModal from "../templates/CreateRegionModal.js";
 
 const RegionCity = async () => {
@@ -7,16 +10,20 @@ const RegionCity = async () => {
     let section = document.createElement('section');
     section.classList.add('regions-table' , 'col-9');
 
-    const regions = await region.getAllRegions();
+    const [ regions , countries , cities ] = await Promise.all([region.getAll(), country.getAll() , city.getAll()]);    
 
-    const openModal = (e,title) => {     
-        
+    const openModal = ( e , title ) => {     
+
         const h2 = document.querySelector('.title-modal');
 
         if (e.target.tagName === 'BUTTON') {
-            h2.textContent = title === 'pais' ? `Nuevo ${title}` : `Nueva ${title}`;
+            h2.textContent = title.includes('pais') || title.includes('país') ? `Nuevo ${title}` : `Nueva ${title}`;
             document.querySelector('#name').value = ''
+            
+            const parentID = e.target.parentNode.parentNode.parentNode.parentNode.id;
+            h2.id = title.includes('pais') || title.includes('país') ? 'ncountry-' + parentID : 'ncity-' + parentID;
         } 
+
         else h2.textContent = title === 'pais' ? `Modificar ${title}` : `Modificar ${title}`;
 
         const modal = document.querySelector('.modal-user');
@@ -29,17 +36,67 @@ const RegionCity = async () => {
         <div class=title-content>
             <h2 class="">Regiones</h2>
         </div>
-        <div class="accordion" id="accordionPanelsStayOpenExample">
+        <div class="accordion mb-5" id="accordionPanelsStayOpenExample">
         </div>       
     `;
 
+    const titleContent = ( title , textButton , textModal ) => {
+        
+        const div = document.createElement('div');
+        div.classList.add('title-content' , 'section');
+
+        const h3Title = document.createElement('h3');
+        h3Title.textContent = title;
+
+        const createButton = document.createElement('button');
+        createButton.textContent = textButton;
+        createButton.classList.add('add-button');
+        createButton.onclick = (e) => openModal( e , textModal );
+
+        div.appendChild(h3Title);
+        div.appendChild(createButton);
+
+        return div
+    }
+
     section.innerHTML = view;
-    regions.data.map( region => section.children[1].appendChild(AccordionItem( region , openModal ))); // Create each region item, pass openModal to execute by clicking edit button 
+    const accordionContainer = section.children[1];
+    let regionCounter = 0;
+    regions.data.forEach( region  => {
+
+        let countryCounter = 0; // Reset count, countries per region
+
+        const countryHeader = titleContent( region.name , 'Agregar país' , `país en ${region.name}`);
+        accordionContainer.appendChild(AccordionItem( region , openModal , "region" ));
+        const countriesContainer = accordionContainer.children[regionCounter].children[1].children[0];
+        countriesContainer.appendChild(countryHeader);
+
+        countries.data.forEach ( country  => {
+
+            if ( country.region_id === region.id ) {
+
+                   
+                countriesContainer.appendChild( AccordionItem( country , openModal , "country" ) ); 
+                countryCounter++; 
+
+                cities.data.forEach ( city => {
+                    
+                    if ( city.country_id === country.id) {
+                    
+                        const citiesContainer = countriesContainer.children[countryCounter].children[1].children[0];
+                        citiesContainer.appendChild( AccordionItem( city , openModal , "city" ) );
+                    }
+            
+                });
+            };
+        });
+        regionCounter++;
+    });
 
     const createRegionButton = document.createElement('button');
     createRegionButton.textContent = 'Agregar región';
     createRegionButton.classList.add('add-button');
-    createRegionButton.onclick = (e) => openModal( e , 'region' );
+    createRegionButton.onclick = (e) => openModal( e , 'región' );
 
     section.children[0].insertBefore(createRegionButton, section.children[0].children[1]);
     section.appendChild(CreateRegionModal());
