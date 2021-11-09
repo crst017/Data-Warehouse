f80704443c4c5b8294929885e41e9af0b7387378
cristian navarro leon <cristiannl.0117@gmail.com>
2021-11-09 00:55:14 -0500

    add parents id info to create a new country / city

--- Front/templates/CreateRegionModal.js
+++ Front/templates/CreateRegionModal.js
@@ -1,43 +1,53 @@
 import validateRegionModalData from "../utils/validateRegionModalData.js";
 import region from "../utils/handleRegions.js";
-import configAlerts from "../utils/configAlerts.js";
+import country from "../utils/handleCountries.js";
+import city from "../utils/handleCities.js";
 
+import configAlerts from "../utils/configAlerts.js";
 const CreateRegionModal = () => {
 
-    const validateRequest = ( response ) => {
+    const validateRequest = ( response , title ) => {
         const method = response.config.method;
         if ( response.status === 201) {
             method === 'put' ? 
-                configAlerts.modifyConfirm( 'región' , 'modificada') :
-                configAlerts.modifyConfirm( 'región' , 'creada');
+                configAlerts.modifyConfirm( title.split(' ')[1] , 'modificada') :
+                configAlerts.modifyConfirm( title.split(' ')[1] , 'creada');
         } else {
             method === 'put' ?
-                configAlerts.modifyError( 'región' , 'modificar') :
-                configAlerts.modifyError( 'región' , 'crear' , response.data);
+                configAlerts.modifyError( title.split(' ')[1] , 'modificar') :
+                configAlerts.modifyError( title.split(' ')[1] , 'crear' , response.data);
         };
         document.querySelector('.modal-user').classList.remove('visible');
     }
 
     const userAction = async (e) => {
-        
+
         const title = document.querySelector('.title-modal');
         const action = title.textContent;
-        const regionID = title.id;
+        const infoID = title.id.split('-')[0]; 
+        const itemID = title.id.split('-')[1];
+
+        const endpoint = action.includes("región") || action.includes("region") ? region  :
+                         action.includes("país")   || action.includes("pais")   ? country : city;
         
         e.preventDefault();
-        const validatedData = validateRegionModalData();
-  
-        if ( validatedData )  {
 
+        const validatedData = validateRegionModalData();
+
+        if ( validatedData )  {
             let data = validatedData;
             if (action.includes('Nuev')) {
-                const response = await region.createRegion(data);
-                validateRequest( response );       
+                
+                if ( infoID === 'ncountry') data = { ...data , region_id : itemID }  // In this case, the parent ID required for the endpoint;
+                if ( infoID === 'ncity')    data = { ...data , country_id : itemID } // In this case, the parent ID required for the endpoint;
+                const response = await endpoint.create(data);
+                validateRequest( response , action );       
             }
             else {
-                data = { ...data , regionID }
-                const response = await region.editRegion(data);
-                validateRequest( response );
+
+                data = { ...data , itemID }
+                const response = await endpoint.edit(data);
+                validateRequest( response , action );
             }
         }
     }
