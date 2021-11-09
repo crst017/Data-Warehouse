80f3061b7840c86b9e6aa27f7cb8402606b59040
cristian navarro leon <cristiannl.0117@gmail.com>
2021-11-09 00:59:48 -0500

    add generalized endpoint execution

--- Front/templates/AccordionItem.js
+++ Front/templates/AccordionItem.js
@@ -1,48 +1,60 @@
-import region from '../utils/handleRegions.js'
+import region from '../utils/handleRegions.js';
+import country from '../utils/handleCountries.js';
+import city from '../utils/handleCities.js';
+
 import configAlerts from '../utils/configAlerts.js';
 
-const AccordionItem = ( item , openModal) => {
+const AccordionItem = ( item , openModal , itemCategory ) => {
 
-    const deleteUser = async (e) => {
+    itemCategory = itemCategory.toLowerCase();
 
-        const regionID = e.target.parentNode.parentNode.parentNode.id;
+    const endpoint = itemCategory === "region"  ? region  :
+                     itemCategory === "country" ? country : city;
+
+    const title = itemCategory === "region"  ? "región" :
+                  itemCategory === "country" ? "país"   : "ciudad";
+
+    const deleteItem = async ( e ) => {
+
+        const categoryID = e.target.parentNode.parentNode.parentNode.id;
 
         Swal.fire( configAlerts.deleteAlert )
             .then( async (result) => {
                 if (result.isConfirmed) {
                     try {
-                        const response = await region.deleteRegion(regionID);
+                        const response = await endpoint.delete( categoryID );
                         response.status !== 405 ?
-                            configAlerts.modifyConfirm( 'region' , 'eliminado') :
-                            configAlerts.modifyError( 'region' , 'eliminar');
+                            configAlerts.modifyConfirm( title , 'eliminado') :
+                            configAlerts.modifyError( title , 'eliminar' , response.data.sqlMessage ); // Send string with error info
                     } catch (error) {
-                        configAlerts.modifyError( 'region' , 'eliminar');
+                        configAlerts.modifyError( title , 'eliminar');
                     }
                 }
             })
     }
 
-    const editUser = async (e) => {
-        const regionID = e.target.parentNode.parentNode.parentNode.id; // Same id as row id (user id)
-        document.querySelector('.title-modal').id = regionID; // Passing the id using the title
-        
-        const regionInfo = await region.getRegion(regionID);
+    const editItem = async ( e ) => {
+
+        const categoryID = e.target.parentNode.parentNode.parentNode.id; // Same id as row id (user id)
+        document.querySelector('.title-modal').id = title + "-" + categoryID; // Passing the id using the title
+
+        const categoryInfo = await endpoint.get( categoryID );
         const name = document.querySelector('#name');
-        name.value = regionInfo.data.name;
-        openModal(e,'region'); // The endpoint is accesed through modal button. See CreateRegionModal.js
+        name.value = categoryInfo.data.name;
+        openModal( e , title ); // The endpoint is accesed through modal button. See CreateRegionModal.js
     }
 
     const createEdit = () => {
         const modifyUserButton = document.createElement('span');
         modifyUserButton.classList.add('icon-mode_edit');
-        modifyUserButton.onclick = editUser;
+        modifyUserButton.onclick = editItem;
         return modifyUserButton;
     }
 
     const createDelete = () => {
         const deleteUserButton = document.createElement('span');
         deleteUserButton.classList.add('icon-delete');
-        deleteUserButton.onclick = deleteUser;
+        deleteUserButton.onclick = deleteItem;
         return deleteUserButton;
     }
 
@@ -51,15 +63,15 @@ const AccordionItem = ( item , openModal) => {
             <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse${item.id}" 
                 aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
                 ${item.name}
-                
             </button>
             <div class="actions">
                 <span class="icon-dots-three-horizontal"></span>
             </div>
         </h2>
         <div id="panelsStayOpen-collapse${item.id}" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
-        <div class="accordion-body w-75">
-            <strong>This is the first item's accordion body.</strong></div>
+            <div class="accordion-body">
+                
+            </div>
         </div>
     `;
 
